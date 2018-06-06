7dd7570007688f1540ffc0ae49365e5f1847bf58
Sarah Jacob <smjacob91@gmail.com>
2018-06-06 17:46:43 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -18,7 +18,7 @@ describe('Fix the Scope', function() {
   })
 
   describe('add2(n)', function() {
-    xit('adds two to n', function() {
+    it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
     })
